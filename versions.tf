2f5105eb30cc2a212b8830d59cb73dfbc96cb5f4
Azat Safin <azsafin@provectus.com>
2022-09-19 14:06:16 +0300

    remove unnecessary providers

--- versions.tf
+++ versions.tf
@@ -16,14 +16,6 @@ terraform {
       source  = "hashicorp/null"
       version = "~> 3.1.1"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.3.1"
-    }
-    template = {
-      source  = "hashicorp/template"
-      version = "~> 2.2.0"
-    }
     docker = {
       source  = "kreuzwerker/docker"
       version = "2.18.0"
