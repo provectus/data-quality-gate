2f201a737abbe817aefa84fac1fe6e884b4381a0
bvolodarskiy <bvolodarskiy@provectus.com>
2023-02-10 16:44:47 +0300

    optimized allure docker

--- functions/allure_report/generate_report.sh
+++ functions/allure_report/generate_report.sh
@@ -1,8 +1,6 @@
 FOLDER=$1
 BUCKET=$2
-rm -r ../tmp/result
-rm -r ../tmp/allure-report
 echo "$BUCKET"
-aws s3 sync s3://"$BUCKET"/allure/"$FOLDER"/result ../tmp/result
-allure/allure-2.14.0/bin/allure generate ../tmp/result --clean -o ../tmp/allure-report
-aws s3 sync ../tmp/allure-report s3://"$BUCKET"/allure/"$FOLDER"/allure-report
+aws s3 sync s3://"$BUCKET"/allure/"$FOLDER"/result /tmp/result
+allure/allure-2.14.0/bin/allure generate /tmp/result --clean -o /tmp/allure-report
+aws s3 sync ./tmp/allure-report s3://"$BUCKET"/allure/"$FOLDER"/allure-report
