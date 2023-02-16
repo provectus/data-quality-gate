3149d81d084ee4cf122b132bc296ae88d547cacf
Aleksei Chumagin <a-chumagin@users.noreply.github.com>
2023-02-16 16:31:51 +0300

    Data test parquet (#38)
    * feat: added parquet test
    
    * feat: make tests parametrize
    
    * fix: added fixture name
    
    * feat: added trigger by commit to existed PR

--- tests/integration_tests/test_data_tests/test_data_test.py
+++ tests/integration_tests/test_data_tests/test_data_test.py
@@ -3,65 +3,41 @@ import awswrangler as wr
 import os
 from jsonschema import validate
 import boto3
+import pytest
+
+schema = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+        "path": {"type": "array", "items": [{"type": "string"}]},
+        "file": {"type": "string"},
+        "profiling": {"type": "string"},
+        "test_suite": {"type": "string"},
+        "suite_name": {"type": "string"},
+        "folder_key": {"type": "string"},
+        "run_name": {"type": "string"},
+        "validate_id": {"type": "string"}
+    },
+    "required": [
+        "path", "file", "profiling", "test_suite", "suite_name",
+        "folder_key", "run_name", "validate_id"
+    ]
+}
 
 
-def test_data_test_csv():
+@pytest.fixture(scope="function")
+def s3_test_data(request):
     url = f"http://{os.environ['S3_HOST']}:4566"
     wr.config.s3_endpoint_url = url
-    schema = {
-      "$schema": "http://json-schema.org/draft-04/schema#",
-      "type": "object",
-      "properties": {
-        "path": {
-          "type": "array",
-          "items": [
-            {
-              "type": "string"
-            }
-          ]
-        },
-        "file": {
-          "type": "string"
-        },
-        "profiling": {
-          "type": "string"
-        },
-        "test_suite": {
-          "type": "string"
-        },
-        "suite_name": {
-          "type": "string"
-        },
-        "folder_key": {
-          "type": "string"
-        },
-        "run_name": {
-          "type": "string"
-        },
-        "validate_id": {
-          "type": "string"
-        }
-      },
-      "required": [
-        "path",
-        "file",
-        "profiling",
-        "test_suite",
-        "suite_name",
-        "folder_key",
-        "run_name",
-        "validate_id"
-      ]
-    }
     b_name = "dataqa"
-    file_name = "titanic.csv"
+    file_name = request.param
     file_path = f"{b_name}/{file_name}"
     local_path = f"./test_data/{file_name}"
     event = {
-      "run_name": "local_test",
-      "source_root": b_name,
-      "source_data": file_path,
-      "engine": "s3"
+        "run_name": "local_test",
+        "source_root": b_name,
+        "source_data": file_path,
+        "engine": "s3"
     }
     s3 = boto3.resource("s3", endpoint_url=url)
     qa_bucket_name = os.environ['QA_BUCKET']
@@ -72,3 +48,10 @@ def test_data_test_csv():
     s3.Object(b_name, file_path).put(Body=open(local_path, 'rb'))
     result = data_test.handler(event, {})
     validate(instance=result, schema=schema)
+    yield file_path
+    s3.Object(b_name, file_path).delete()
+
+
+@pytest.mark.parametrize("s3_test_data", ["titanic.csv", "titanic.parquet"], indirect=True)
+def test_data_test(s3_test_data):
+    pass
