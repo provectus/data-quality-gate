345367890d4f622087f05f24e02ebbdf4bacc526
egorodov <119041467+egorodov@users.noreply.github.com>
2023-04-11 12:38:00 +0300

    Basic example v2 (#54)
    * move state to s3
    
    * minor fixes for ci script

---
+++ examples/basic/remote_state.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "fast-data-qa-terraform"
+    key            = "terraform/state/terraform.tfstate"
+    region         = "eu-west-2"
+    encrypt        = true
+  }
+}
