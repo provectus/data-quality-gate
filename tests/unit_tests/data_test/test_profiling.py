e7f0b9873c153b599c6701903e3f1b1018e33322
achumagin <achumagin@hotmail.com>
2023-05-11 13:46:54 +0300

    fix: reformat test_profiling

--- tests/unit_tests/data_test/test_profiling.py
+++ tests/unit_tests/data_test/test_profiling.py
@@ -5,7 +5,10 @@ from functions.data_test.profiling import (add_local_s3_to_stores,
 
 @pytest.mark.parametrize("stores, expected_output", [
     ({"store1": {"store_backend": {"type": "s3", "bucket": "my-bucket"}}},
-     {"store1": {"store_backend": {"type": "s3", "bucket": "my-bucket", "boto3_options": {"endpoint_url": "http://localhost:4566"}}}}),
+     {"store1": {"store_backend": {"type": "s3", "bucket": "my-bucket",
+                                   "boto3_options":
+                                   {"endpoint_url": "http://localhost:4566"}
+                                   }}}),
     ({}, {})
 ])
 def test_add_local_s3_to_stores(stores, expected_output):
