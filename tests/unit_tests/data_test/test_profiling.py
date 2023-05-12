d39f24c9e2ed51061a47717262b82d31d3a2be28
achumagin <achumagin@hotmail.com>
2023-05-12 14:15:43 +0300

    fix: added constant

--- tests/unit_tests/data_test/test_profiling.py
+++ tests/unit_tests/data_test/test_profiling.py
@@ -2,18 +2,18 @@ import pytest
 from functions.data_test.profiling import (add_local_s3_to_stores,
                                            read_gx_config_file)
 
+ENDPOINT_URL = "http://localhost:4566"
+
 
 @pytest.mark.parametrize("stores, expected_output", [
     ({"store1": {"store_backend": {"type": "s3", "bucket": "my-bucket"}}},
      {"store1": {"store_backend": {"type": "s3", "bucket": "my-bucket",
                                    "boto3_options":
-                                   {"endpoint_url": "http://localhost:4566"}
-                                   }}}),
+                                   {"endpoint_url": ENDPOINT_URL}}}}),
     ({}, {})
 ])
 def test_add_local_s3_to_stores(stores, expected_output):
-    endpoint_url = "http://localhost:4566"
-    assert add_local_s3_to_stores(stores, endpoint_url) == expected_output
+    assert add_local_s3_to_stores(stores, ENDPOINT_URL) == expected_output
 
 
 def test_gx_config_file():
