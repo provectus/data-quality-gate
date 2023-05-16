a9bbd951f1b3680515d3813161ae66a8d39c0283
Aleksei Chumagin <a-chumagin@users.noreply.github.com>
2023-05-16 17:15:08 +0300

    [MAINTAINCE] read config file instead using datacontext (#74)
    * feat: read config file

--- tests/unit_tests/data_test/test_profiling.py
+++ tests/unit_tests/data_test/test_profiling.py
@@ -1,12 +1,28 @@
 import pytest
-from functions.data_test.profiling import (add_local_s3_to_stores)
+from functions.data_test.profiling import (add_local_s3_to_stores,
+                                           read_gx_config_file)
+
+ENDPOINT_URL = "http://localhost:4566"
 
 
 @pytest.mark.parametrize("stores, expected_output", [
     ({"store1": {"store_backend": {"type": "s3", "bucket": "my-bucket"}}},
-     {"store1": {"store_backend": {"type": "s3", "bucket": "my-bucket", "boto3_options": {"endpoint_url": "http://localhost:4566"}}}}),
+     {"store1": {"store_backend": {"type": "s3", "bucket": "my-bucket",
+                                   "boto3_options":
+                                   {"endpoint_url": ENDPOINT_URL}}}}),
     ({}, {})
 ])
 def test_add_local_s3_to_stores(stores, expected_output):
-    endpoint_url = "http://localhost:4566"
-    assert add_local_s3_to_stores(stores, endpoint_url) == expected_output
+    assert add_local_s3_to_stores(stores, ENDPOINT_URL) == expected_output
+
+
+def test_gx_config_file():
+    config_file = read_gx_config_file()
+    assert config_file["config_version"] == 2.0
+
+
+def test_gx_config_file_path_is_not_none(tmpdir):
+    p = tmpdir.mkdir("config").join("great_expectations.yml")
+    p.write("config_version: 10.0")
+    config_file = read_gx_config_file(path=p)
+    assert config_file["config_version"] == 10.0
