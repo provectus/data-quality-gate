6d0b47ca2a84b3f4ac1632889a5439138159a446
achumagin <achumagin@hotmail.com>
2023-05-26 18:15:15 +0300

    feat: fix version

--- tests/integration_tests/infra/main.tf
+++ tests/integration_tests/infra/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region              = "us-west-2"
   access_key          = "local-access-key"
   secret_key          = "local-secret-key"
+  s3_force_path_style = true
 
   endpoints {
     s3  = "http://localhost:4566"
