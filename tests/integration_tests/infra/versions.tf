a0ce57fba4406f95a97dad7b7dc7e8ef8c359bad
achumagin <achumagin@hotmail.com>
2023-05-26 18:50:57 +0300

    fix: typo

--- tests/integration_tests/infra/versions.tf
+++ tests/integration_tests/infra/versions.tf
@@ -1,7 +1,10 @@
-
-required_providers {
-  aws = {
-    source  = "hashicorp/aws"
-    version = "= 4.67.0"
+terraform {
+  required_version = ">= 1.1.7"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 4.67.0"
+    }
   }
 }
