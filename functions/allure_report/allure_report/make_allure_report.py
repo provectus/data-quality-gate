e6713ae8ebef6ce74f29b36fc10f8f1dd93eecbf
bvolodarskiy <bvolodarskiy@provectus.com>
2023-08-02 12:15:41 +0300

    added logging for allure_report

--- functions/allure_report/allure_report/make_allure_report.py
+++ functions/allure_report/allure_report/make_allure_report.py
@@ -1,8 +1,9 @@
 from mapper import create_json_report
 import os
-
+from loguru import logger
 
 def handler(event, context):
+    logger.info("Starting making of allure report")
     qa_bucket = os.environ['BUCKET']
     reports_web = os.environ['REPORTS_WEB']
     report = event['report'].get('Payload')
@@ -12,5 +13,6 @@ def handler(event, context):
     link, key = create_json_report(suite, reports_web, folder_key, validate_id)
     os.system("chmod +x generate_report.sh")
     os.system(f"sh generate_report.sh {key} {qa_bucket}")
+    logger.info("Making of allure report is finished")
 
     return link
